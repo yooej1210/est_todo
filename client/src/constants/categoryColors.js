2355cc1f8bb5e4b19eaffb500af1f2ce825a01f4
yooej <yooej1210@gmail.com>
2026-01-08 19:46:36 +0900

    카테고리 컬러 차트

---
+++ client/src/constants/categoryColors.js
@@ -0,0 +1,11 @@
+export const CATEGORY_COLORS = [
+  { name: "Gray", hex: "#E3E3E1" },
+  { name: "Brown", hex: "#EADDD8" },
+  { name: "Orange", hex: "#F6D7B8" },
+  { name: "Yellow", hex: "#F3E5A6" },
+  { name: "Green", hex: "#DCE8DA" },
+  { name: "Blue", hex: "#D6EAF3" },
+  { name: "Purple", hex: "#E8E0F0" },
+  { name: "Pink", hex: "#F2DCE6" },
+  { name: "Red", hex: "#F4D0CC" },
+];
